1681e3c1bfe150a0a3a4f0423c8a36b25f263d4e
DESKTOP-K6IHCUD\Sahan_Kumarage <kavishkasahandj@gmail.com>
2024-01-26 16:20:22 +0530

    added pie chart

--- Pages/History/history.js
+++ Pages/History/history.js
@@ -1,8 +1,8 @@
 const data = {
     datasets: [{
         label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+        data: [300, 100],
+        backgroundColor: ['#f61c09', '#78cd32'],
         hoverOffset: 4,
     }],
 };
